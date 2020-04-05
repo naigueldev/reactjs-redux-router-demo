566248fbcc642cfcd8b3ace2ac4ec2d3aa993583
Naiguel Santos <naiguelsantosdeveloper@gmail.com>
2020-04-05 08:07:45 -0300

    Fix conflicts

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,17 +9,12 @@ export default function Home() {
           <div className="auth-wrapper">
             <div className="auth-inner col-12">
               <h1 className="display-4 mb-4">ReactJS - Projeto Demo</h1>
-<<<<<<< HEAD
               <p className="mb-0">
                 Projeto demo criado para implementação e utilização de:
               </p>
               <p className="lead">
                 Redux, Router, Axios, Bootstrap, Pages e Components
               </p>
-=======
-              <p className="mb-0">Projeto demo criado para implementação de:</p>
-              <p className="lead">Redux, Router, Axios e Bootstrap</p>
->>>>>>> a2a16a67fc01b04fe8f220931ed5e37667a4e987
             </div>
           </div>
         </Col>
